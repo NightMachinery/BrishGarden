d016da7a05ce8113a0d0eb4e00d4537116d5bd02
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-26 23:16:49 +0430

    switched to POST

--- garden.py
+++ garden.py
@@ -13,8 +13,17 @@ brishes = [brish.Brish() for i in range(4)]
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/zsh/")
-def cmd_zsh(cmd: str, verbose: Optional[int] = 0):
+@app.post("/test/")
+def test(body: dict):
+    return body
+
+@app.post("/zsh/")
+def cmd_zsh(body: dict):
+    # GET Method: cmd: str, verbose: Optional[int] = 0
+    cmd = body.get('cmd', '')
+    if cmd == '':
+        return Response(content="Empty command received.", media_type="text/plain")
+    verbose = int(body.get('verbose', 0))
     while len(brishes) <= 0:
         time.sleep(1)
     myBrish = brishes.pop()
@@ -24,3 +33,5 @@ def cmd_zsh(cmd: str, verbose: Optional[int] = 0):
         return Response(content=res.outerr, media_type="text/plain")
     else:
         return {"cmd": cmd, "brishes": len(brishes), "out": res.out, "err": res.err, "retcode": res.retcode}
+
+# Use `pass: str`, hash it a lot along BRISHGARDEN_SALT, and compare to BRISHGARDEN_PASS. Abort if any of the two vars are empty.
