531a2c9775831d4ca96f1f5f10ae3289c154c531
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-27 20:04:11 +0430

    Added support for setting root_path by using the first CLI arg

--- brishgarden/__init__.py
+++ brishgarden/__init__.py
@@ -1,5 +1,8 @@
-import uvicorn
+import uvicorn, sys
 # from .garden import app
 
 def main():
-    uvicorn.run("brishgarden.garden:app", host="127.0.0.1", port=7230, log_level="info", proxy_headers=True)
+    root_path = ''
+    if len(sys.argv) >= 2:
+        root_path = sys.argv[1]
+    uvicorn.run("brishgarden.garden:app", host="127.0.0.1", port=7230, log_level="info", proxy_headers=True, root_path=root_path)
