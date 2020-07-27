fef5fd29538a2dea0d3a690d34995acd088e1c84
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-27 21:39:30 +0430

    Allows setting stdin.

--- brishgarden/garden.py
+++ brishgarden/garden.py
@@ -34,18 +34,21 @@ async def get_ip(request: Request):
 @app.post("/zsh/")
 def cmd_zsh(body: dict):
     # GET Method: cmd: str, verbose: Optional[int] = 0
+    # body: cmd [verbose: int=0,1] [stdin: str]
     cmd = body.get('cmd', '')
     if cmd == '':
         return Response(content="Empty command received.", media_type="text/plain")
+    stdin = body.get('stdin', '')
     verbose = int(body.get('verbose', 0))
     while len(brishes) <= 0:
         time.sleep(1)
     myBrish = brishes.pop()
-    res = myBrish.z(cmd)
+    res = myBrish.send_cmd(cmd, fork=False, cmd_stdin=stdin)
     brishes.append(myBrish)
     if verbose == 0:
         return Response(content=res.outerr, media_type="text/plain")
     else:
         return {"cmd": cmd, "brishes": len(brishes), "out": res.out, "err": res.err, "retcode": res.retcode}
 
+# Old security scheme: (We now use Caddy's HTTP auth.)
 # Use `pass: str`, hash it a lot along BRISHGARDEN_SALT, and compare to BRISHGARDEN_PASS. Abort if any of the two vars are empty. We probably need to answer the query right away for this security model to work, because hashing necessarily needs to be expensive.
